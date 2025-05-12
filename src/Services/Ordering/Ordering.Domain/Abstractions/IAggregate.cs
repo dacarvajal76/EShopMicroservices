cadf1c8ab1dc2cf596f335bfa2c80bd482ab060f
dacarvajal.76 <dacarvajal.76@gmail.com>
2025-05-12 17:58:33 -0400

    Implementing the Domain and starting to implement Entity Framework for Ordering

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Ordering.Domain.Abstractions
+{
+
+    public interface IAggregate<T> : IAggregate, IEntity<T>
+    {
+        // Marker interface for aggregates
+        // This interface inherits from IAggregate and IEntity<T>
+        // to enforce the implementation of both interfaces in aggregate classes.
+    }
+    public interface IAggregate : IEntity
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents { get; }
+
+        IDomainEvent[] ClearDomainEvents();
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
